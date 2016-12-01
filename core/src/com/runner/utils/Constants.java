f5391474d405a572fea9f8ca862b2f159adac3f0
Bob <jayjay93@gmx.de>
2016-12-01 09:56:43 +0100

    UI files changed

--- core/src/com/runner/utils/Constants.java
+++ core/src/com/runner/utils/Constants.java
@@ -64,8 +64,8 @@ public class Constants {
     public static final String BACKGROUND_IMAGE_PATH = "background.png";
     public static final String GROUND_IMAGE_PATH = "ground.png";
 
-    public static final String UI_ATLAS_PATH = "ui/atlas.txt";
-    public static final String UI_SKIN_PATH = "ui/skin.json";
+    public static final String UI_ATLAS_PATH = "ui/uiatlas.txt";
+    public static final String UI_SKIN_PATH = "ui/uiskin.json";
 
     public static final String CHARACTERS_ATLAS_PATH = "characters.txt";
     public static final String[] RUNNER_RUNNING_REGION_NAMES = new String[] {"alienGreen_run1", "alienGreen_run2"};
